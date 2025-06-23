53ce3624a36830791bc239881d514933f9a1c349
Omnath Shinde <omnath@briot.in>
2025-06-23 09:02:21 +0530

    Add sass for style and can use src as root dirctory

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "src/App.scss";
 
 function App() {
 	const api = import.meta.env.VITE_API_URL;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
 	envDir: "./environments",
 	plugins: [react()],
+	resolve: {
+		alias: {
+			src: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+		},
+	},
 });
